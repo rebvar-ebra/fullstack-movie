e0e471751fbc4ac2a82ab64ad4ab4bdbf9c2287a
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2023-03-15 16:16:59 +0100

    service

---
+++ src/main/java/tech/rebvar/movies/MovieRepoistory.java
@@ -0,0 +1,12 @@
+package tech.rebvar.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepoistory extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
---
+++ src/main/java/tech/rebvar/movies/MovieService.java
@@ -0,0 +1,21 @@
+package tech.rebvar.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepoistory movieRepository;
+
+    public List<Movie> findAllMovies() {
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
